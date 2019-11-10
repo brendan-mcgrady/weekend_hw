6c24a138da4ca5a2618252e487466b050059caf1
Brendan <laotsu@hotmail.co.uk>
2019-11-10 19:02:07 +0000

    Add cash function passed, thanks Paul

--- pet_shop.rb
+++ pet_shop.rb
@@ -5,3 +5,8 @@ end
 def total_cash(shop_name)
   shop_name[:admin][:total_cash]
 end
+
+def add_or_remove_cash(shop, amount)
+  cash = shop[:admin][:total_cash]
+  shop[:admin][:total_cash] = cash + amount
+end

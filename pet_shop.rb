64c93e150e80de90951190eec4e904a06341928c
Brendan <laotsu@hotmail.co.uk>
2019-11-10 22:28:45 +0000

    Syntax refactor

--- pet_shop.rb
+++ pet_shop.rb
@@ -7,8 +7,7 @@ def total_cash(shop_name)
 end
 
 def add_or_remove_cash(shop, amount)
-  cash = shop[:admin][:total_cash]
-  shop[:admin][:total_cash] = cash + amount
+  return shop[:admin][:total_cash] += amount
 end
 
 def pets_sold(sold_amount)
@@ -16,10 +15,20 @@ def pets_sold(sold_amount)
 end
 
 def increase_pets_sold(pets_sold, amount)
-  sold_num = pets_sold[:admin][:pets_sold]
-  pets_sold[:admin][:pets_sold] = sold_num + amount
+  return pets_sold[:admin][:pets_sold] += amount
 end
 
 def stock_count(stock_total)
   return stock_total[:pets].count()
 end
+
+# def pets_by_breed(shop, breed)
+#
+#     for breed_name in shop[:pets]
+#       if breed[:pets] == breed_name
+#         return breed.to_i()
+#       else
+#         return false
+#       end
+#     end
+# end
